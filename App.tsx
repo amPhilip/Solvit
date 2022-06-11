9269c8fc2ebc14e3cb4923a31b4588bf63917a66
amPhilip <philipmwangi91@gmail.com>
2022-06-11 03:46:36 +0300

    solvit app:Theme functionality

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Switch } from "react-native";
+import { Theme } from "./context/Theme";
+import { appColors } from "./src/components/styles/appColors";
 
 export default function App() {
+  const [theme, setTheme] = useState("light");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Theme.Provider value={theme}>
+      <View
+        style={
+          theme === "light"
+            ? styles.container
+            : [styles.container, { backgroundColor: "#1E1F22" }]
+        }
+      >
+        <StatusBar style="auto" />
+        <Switch
+          value={theme === "light"}
+          onValueChange={() => setTheme(theme === "light" ? "dark" : "light")}
+        />
+      </View>
+    </Theme.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: appColors.light,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
